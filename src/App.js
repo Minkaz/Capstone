d30f0c86eacc64a7ccfb80cb5a83e5387d7ad8c7
Mindaugas <kazukauskas.mindaugas@gmail.com>
2023-03-05 21:51:18 +0200

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './routes/home/home.component';
+import { Routes, Route} from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const Shop = () => {
+        return <h1>Shop page</h1>;
+    }
+
+    return  (
+        <Routes>
+            <Route path={'/'} element={<Navigation />}>
+                <Route index element={<Home />}></Route>
+                <Route path={'shop'} element={<Shop />}></Route>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
