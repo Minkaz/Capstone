b57816ec39702e70c41dfa3342d5587918d25385
Mindaugas <kazukauskas.mindaugas@gmail.com>
2023-03-17 20:08:00 +0200

    Added user context

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,8 +1,9 @@
-import {useState} from "react";
+import {useState, useContext} from "react";
 import FormInput from '../form-input/form-input.component';
 import './sign-in-form.component.scss';
 import Button from '../button/button.component';
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+import {UserContext} from "../../contexts/user.context";
 
 const defaultFormFields = {
     password: '',
@@ -13,6 +14,7 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const {email, password} = formFields;
 
+    const { setCurrentUser } = useContext(UserContext);
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
@@ -21,7 +23,9 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-            const response = await signInAuthUserWithEmailAndPassword(email, password);
+            const {user} = await signInAuthUserWithEmailAndPassword(email, password);
+            setCurrentUser(user);
+
             resetFormFields();
         } catch (e) {
             switch (e.code) {
@@ -35,7 +39,6 @@ const SignInForm = () => {
                     console.log(e);
                     break;
             }
-            
         }
     }
 
